89c9b686f87ea0704588c4f135ce38fc2e8348ab
HPGOD <67414205+HPG0D@users.noreply.github.com>
2021-03-31 14:28:07 -0100

    Added a gif/png loader too it.

--- ViewerColor.user.js
+++ ViewerColor.user.js
@@ -1,89 +1,148 @@
 // ==UserScript==
 // @name         Krunker Viewer Color Picker
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      2.0
 // @description  Enables to change the background color of the viewer
 // @author       BudgetArms
 // @match        https://krunker.io/viewer.html*
 // @grant        none
 // ==/UserScript==
 ! function () {
-  setTimeout(Hey, 100);
+  setTimeout(Hey, 400);
 }();
 function Hey() {
-    var DivGif = document.createElement("div");
-    var SpanGif = document.createElement("span");
-    SpanGif.className = "property-name";
-    var SpanTextGif = document.createTextNode("Load Gif");
-    var li = document.createElement("li");
-    var Div1 = document.createElement("div");
-    var Span = document.createElement("span");
+
+    const li = document.createElement("li");
+    li.className = "cr function";
+
+    const Div1 = document.createElement("div");
+
+    const Span = document.createElement("span");
     Span.className = "property-name";
-    var SpanText = document.createTextNode("Color Picker");
-    var Div2 = document.createElement("div");
+    const SpanText = document.createTextNode("Color Picker");
+
+    const Div2 = document.createElement("div");
     Div2.className = "c";
-    var ColorPicker = document.createElement("input");
+
+    const ColorPicker = document.createElement("input");
     ColorPicker.type = "color";
-    ColorPicker.id = "COLOR";
-    var Input = document.createElement("input");
-    Input.type = "checkbox";
-    Input.id = "Checkbox";
+    ColorPicker.id = "color";
+
+    const currentDiv = document.querySelector("body > div.dg.ac > div").firstElementChild;
+
     Span.appendChild(SpanText);
     Div1.appendChild(Span);
     Div2.appendChild(ColorPicker);
-    Div2.appendChild(Input);
     Div1.appendChild(Div2);
     li.appendChild(Div1);
-    var currentDiv = document.querySelector("body > div.dg.ac > div").firstElementChild;
     currentDiv.appendChild(li);
-    console.dir(currentDiv);
-    Check();
+
+
+    const currentGifDiv = document.querySelector("body > div.dg.ac > div").firstElementChild;
+
+    const DivGif = document.createElement("div");
+    DivGif.id = "Loader";
+
+    const SpanGif = document.createElement("span");
+    SpanGif.className = "property-name";
+    SpanGif.id= "SpanGif";
+    SpanGif.style.width="316px";
+    SpanGif.style.heigth="38.24px";
+
+    const SpanTextGif = document.createTextNode("Load Background");
+    const Gifli = document.createElement("li");
+    Gifli.className = "cr function";
+
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.style.visibility = 'hidden';
+    input.accept = ".png;.jpg,.gif,.svg";
+    input.id = "input";
+
+    SpanGif.appendChild(SpanTextGif);
+    DivGif.appendChild(SpanGif);
+    DivGif.appendChild(input);
+    Gifli.appendChild(DivGif);
+    currentGifDiv.appendChild(Gifli);
+
+    const currentResetDiv = document.querySelector("body > div.dg.ac > div").firstElementChild;
+
+    const SpanResetDivli = document.createElement('li');
+    SpanResetDivli.className = "cr function";
+
+    const ResetDiv = document.createElement('div');
+
+    const ResetSpan = document.createElement("span");
+    const ResetSpanText = document.createTextNode("Reset Background");
+    ResetSpan.className = "property-name";
+    ResetSpan.appendChild(ResetSpanText);
+    ResetDiv.appendChild(ResetSpan);
+
+
+    SpanResetDivli.appendChild(ResetDiv);
+
+    currentResetDiv.appendChild(SpanResetDivli);
+
+    SpanResetDivli.addEventListener("click", function(){
+        ResetBackground();
+    });
+
+    SpanGif.addEventListener("click", function(){
+        Load();
+    });
+    ColorPicker.addEventListener("change", function(){
+        var Hey = ColorPicker.value
+        Color(Hey);
+    });
 };
-window.Check = function(){
-    var ColorPicker = document.querySelector("#COLOR");
-    var Button = document.querySelector("#Checkbox");
-    if(ColorPicker.value != undefined && Button.checked === true){
-        console.log('The ColorPicker is defined' + ' and ' + 'The Button is checked');
-        Loader(1);
-        console.log(ColorPicker.value);
-    }else if(ColorPicker.value != undefined && Button.checked === false){
-        console.log('ColorPicker is defined' + ' and ' + 'The Button is not checked');
-        Loader(2);
-        console.log(ColorPicker.value);
-    }else if(ColorPicker.value === undefined && Button.checked === true){
-        console.log('ColorPicker is undefined' + ' and ' + 'The Button is checked');
-        Loader(3);
-        console.log(ColorPicker.value);
-    }else if(ColorPicker.value === undefined && Button.checked === false){
-        console.log('ColorPicker is undefined' + ' and ' + 'The Button is not checked');
-        Loader(4);
-        console.log(ColorPicker.value);
-    }
+window.Color = function(Color){
+    const container = document.querySelector("#container");
+    console.log(Color);
+    container.style.background = Color;
 }
+
+window.ResetBackground = function(){
+    const container = document.querySelector("#container");
+    const DefaultColor = "linear-gradient(#7d8fa3, #1b1c1e)";
+    container.style.background = DefaultColor;
+}
+window.Load = function(){
+    var input = document.querySelector('#input');
+    var ColorPicker = document.querySelector("#color");
+    var container = document.querySelector('#container');
+    input.click();
+    input.addEventListener("change", function(){
+        const reader = new FileReader();
+        reader.onload = function(){
+            var img = new Image();
+            if(!reader.result) {
+                console.log("No result");
+            };
+            img.src = reader.result;
+            container.style.background = "no-repeat"
+            container.style.backgroundImage = `url("${img.src}")`;
+            var w = window.innerWidth + 1;
+            var h = window.innerHeight + 1;
+            container.style.backgroundSize = w + 'px ' + h + 'px';
+        }
+        try{
+        reader.readAsDataURL(input.files[0]);
+        }catch{
+            console.log("Why didn't you select a file, dumbass?");
+        }
+    },false);
+}
+
 window.Loader = function(a) {
-    var ColorPicker = document.querySelector("#COLOR");
-    var Button = document.querySelector("#Checkbox");
-    var container = document.querySelector("#container");
-    var DefaultColor = "linear-gradient(#7d8fa3, #1b1c1e)";
+    const ColorPicker = document.querySelector("#color");
+    const container = document.querySelector("#container");
+    const DefaultColor = "linear-gradient(#7d8fa3, #1b1c1e)";
     var color = "";
     if(a===1){
         color = ColorPicker.value;
         container.style.background = color;
     }
-    if(a===2){
-        color = ColorPicker.value;
-        container.style.backgroundImage = DefaultColor;
-    }
-    if(a===3){
-        container.style.backgroundImage = DefaultColor;
-    }
-    if(a===4){
-        container.style.backgroundImage = DefaultColor;
-    }
     ColorPicker.onchange = function(){
-        Check();
-    }
-    Button.onchange = function(){
         Check();
     }
 }
